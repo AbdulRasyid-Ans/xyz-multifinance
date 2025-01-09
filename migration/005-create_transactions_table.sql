8d20d2146a579c23e6028d55c06150a57aa6590c
Stern <stern@pop-os.localdomain>
2025-01-09 22:27:24 +0700

    fix: fix erd and migration query

--- migration/005-create_transactions_table.sql
+++ migration/005-create_transactions_table.sql
@@ -4,8 +4,9 @@ CREATE TABLE IF NOT EXISTS `transactions`(
     `consumer_id` BIGINT UNSIGNED NOT NULL,
     `loan_id` BIGINT UNSIGNED NULL,
     `description` VARCHAR(255) NOT NULL,
+    `amount` DECIMAL(19, 3) NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at` TIMESTAMP NULL,
     FOREIGN KEY (`consumer_id`) REFERENCES `consumers`(`consumer_id`),
     FOREIGN KEY (`loan_id`) REFERENCES `loans`(`loan_id`)
